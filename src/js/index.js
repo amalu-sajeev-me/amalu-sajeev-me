c962e2fbff81a992c4c1ba392c8d9ec8c4ace79b
Amalu Sajeev <amalu.sajeev.me@gmail.com>
2022-03-28 22:20:46 +0530

    added skills and projects

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
+import skills from "../db/skills.js";
+
 (function () {
-  let code = document.getElementById("code-type");
+  let code = document.getElementById("code");
   let text = `‹〴› coding....`;
   for (let i = 0; i < text.length; i++) {
     setTimeout(() => {
@@ -7,3 +9,30 @@
     }, 280 * i);
   }
 })();
+
+function fillWithElements(id, data, options) {
+  const target = document.getElementById(id);
+  const { className = "empty", html } = options;
+  data
+    .map((elem) => {
+      let div = document.createElement(`div`);
+      div.classList.add(className);
+      div.classList.add("tiles");
+      div.innerHTML = html(elem);
+      return div;
+    })
+    .forEach((elem) => target.append(elem));
+}
+
+fillWithElements(`skills`, skills, {
+  className: `skill`,
+  html(skill) {
+    return skill.name;
+  },
+});
+
+fillWithElements(`projects`, ["copola"], {
+  html(project) {
+    return project;
+  },
+});
